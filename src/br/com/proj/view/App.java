fb6835e50e016121556ff8d8372c282c7d2044c2
welnovaesm <wel.miro@hotmail.com>
2022-02-02 16:29:48 -0300

    organizacao de pastas, models, utils e database

--- src/br/com/proj/view/App.java
+++ src/br/com/proj/view/App.java
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 package br.com.proj.view;
-=======
->>>>>>> 893e8f81c16b8fa7441a2478e5a5bfeeecaab57c
 
 public class App {
     public static void main(String[] args) throws Exception {
